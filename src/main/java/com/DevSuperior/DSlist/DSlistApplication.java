4390f67b123d51f57a4ffa72e1a5fef96b471b93
Alex Monteiro <alexxmontteiro@gmail.com>
2024-11-16 08:52:55 -0300

    Properties, Game entity, ORM

--- src/main/java/com/DevSuperior/DSlist/DSlistApplication.java
+++ src/main/java/com/DevSuperior/DSlist/DSlistApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DSlistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.(DSlistApplication.class, args);
+		SpringApplication.run(DSlistApplication.class, args);
 	}
-	
-
 }
