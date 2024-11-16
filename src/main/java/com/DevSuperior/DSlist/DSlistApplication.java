67f6852e9e2dcf9522f98640ca08ab8e43a79938
Alex Monteiro <alexxmontteiro@gmail.com>
2024-11-16 08:42:43 -0300

    Project created

---
+++ src/main/java/com/DevSuperior/DSlist/DSlistApplication.java
@@ -0,0 +1,14 @@
+package com.DevSuperior.DSlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.(DSlistApplication.class, args);
+	}
+	
+
+}
