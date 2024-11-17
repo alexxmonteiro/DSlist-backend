6696973843092f4fbe45549144e0c9dd6d60b276
Alex Monteiro <alexxmontteiro@gmail.com>
2024-11-17 18:21:46 -0300

    CORs config

--- src/main/java/com/DevSuperior/DSlist/projections/GameMinProjection.java
+++ src/main/java/com/DevSuperior/DSlist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.DevSuperior.DSlist.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
